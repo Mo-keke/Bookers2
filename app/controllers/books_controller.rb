24ba78c580282a42abfa43f71c7125ff6782fe2f
Mo-keke <175957589+Mo-keke@users.noreply.github.com>
2024-08-27 07:51:27 +0000

    [Finish] Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,27 +6,39 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
+    @user = User.find(current_user.id)
     @book.user_id = current_user.id
-    @book.save
-    flash[:notice] = 'You have created book successfully.'
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = 'You have created book successfully.'
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def show
-    @book = Book.find(params[:id])
-    @user = User.find(@book.user.id)
+    @book = Book.new
+    @chosen_book = Book.find(params[:id])
+    @user = User.find(@chosen_book.user.id)
   end
 
   def edit
     @book = Book.find(params[:id])
+    unless @book.user_id == current_user.id
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice] = 'You have updated book successfully.'
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:notice] = 'You have updated book successfully.'
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
