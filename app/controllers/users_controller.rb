40538e0f437f1f9bb7391cdc039492c17398e19e
Mo-keke <175957589+Mo-keke@users.noreply.github.com>
2024-08-27 04:09:44 +0000

    [Add] Flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
+    flash[:notice] = 'You have updated user successfully.'
     redirect_to user_path(@user.id)
   end
 
@@ -25,5 +26,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-
 end
